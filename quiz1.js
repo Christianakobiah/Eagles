6812ced20efe6dc445e2961c8e2b8c9129f67615
Wixi105 <ericsampson105@gmai.com>
2019-07-23 13:57:38 +0000

    commit edited files

--- quiz1.js
+++ quiz1.js
@@ -10,7 +10,6 @@ const counter = document.getElementById("counter");
 const timeGauge = document.getElementById("timeGauge");
 const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("scoreContainer");
-const change = document.getElementById("change");
 
 // create our questions
 let questions = [
@@ -122,7 +121,6 @@ start.addEventListener("click",startQuiz);
 // start quiz
 function startQuiz(){
     start.style.display = "none";
-    change.style.display = "none";
     renderQuestion();
     quiz.style.display = "block";
     renderProgress();
