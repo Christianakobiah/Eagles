fe55be876e77ffb5b3cfbff3eeff063f8532eb40
Wixi105 <ericsampson105@gmai.com>
2019-07-24 13:58:08 +0000

    added files

--- quiz2.js
+++ quiz2.js
@@ -213,6 +213,7 @@ function scoreRender(){
     scoreDiv.innerHTML = "<img src="+ img +">";
     scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
     scoreDiv.innerHTML += "<a href ='index.php'>Click to Start As New User .</a> ";
+    
 
    
 }
