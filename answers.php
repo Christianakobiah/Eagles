53fec0efe3d9dda95f476adc305b09a7bd95229b
Wixi105 <ericsampson105@gmai.com>
2019-07-25 09:53:46 +0000

    added answers.css file

--- answers.php
+++ answers.php
@@ -10,11 +10,11 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Quiz Answers.</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="index.css"/>
+    <link rel="stylesheet" href="answers.css"/>
 </head>
 <body>
 <div class="container">
-<div class=""><?php echo "Hello " . $_SESSION['username'] . " these are the answers to all the questions" ?></div>
+<div class="user"><?php echo "Hello " . $_SESSION['username'] . " these are the answers to all the questions" ?></div>
 <div id="answers">
 <dl>
 <div class = "ans">
